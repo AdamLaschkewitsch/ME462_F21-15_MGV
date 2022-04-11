dcdd10f3f3b7278c7d5b1268022916ec8b853dfa
AdamLaschkewitsch <98042645+AdamLaschkewitsch@users.noreply.github.com>
2022-04-11 17:01:09 -0500

    Position Control- untested
    Moved rotation to its own funciton and changed it to use the 3 pole switch with the middle acting as a stop.
    Added position control that sets the rotational origin to the motor position when the mode was switched.
    Added no state for when receiver isn't receiving a signal.

--- SteeringTest/SteeringTest.ino
+++ SteeringTest/SteeringTest.ino
@@ -1,68 +1,79 @@
 #include <PPMReader.h>
 
 // Initialize a PPMReader on digital pin 3 with 8 expected channels.
-byte PPMpin = 3; //must be capable of interupts, which is pin 2 and 3 on nano
-byte channelAmount = 8;
+#define PPMpin 3 //must be capable of interupts, which is pin 2 and 3 on nano
+#define channelAmount 8
+
+//Stepper output info
+#define PUL 6
+#define DIR 8
+#define pulseWidth 20  // microseconds
+
 PPMReader ppm(PPMpin, channelAmount);
 
-byte PUL=6;
-byte DIR=8;
-int pulseWidth = 20;  // microseconds
-int millisbetweenSteps = 250; // milliseconds - or try 1000 for slower steps
-
 void setup() {
-
-    pinMode(PUL,OUTPUT);
-    pinMode(DIR,OUTPUT);
-    Serial.begin(115200);
+  pinMode(PUL, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  Serial.begin(115200);
 }
 //*
-void PULSE(int Step_Hz,int dir){
-    float microbetweenSteps=(1.0/Step_Hz)*1000000;
-    
-    digitalWrite(DIR,dir);
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(pulseWidth);
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(microbetweenSteps-pulseWidth);
-    //Serial.println(microbetweenSteps);
+void PULSE(int Step_Hz, int dir) {
+  float cycleWidth = (1.0 / Step_Hz) * 1000000;
+
+  digitalWrite(DIR, dir);
+  digitalWrite(PUL, HIGH);
+  delayMicroseconds(pulseWidth);
+  digitalWrite(PUL, LOW);
+  delayMicroseconds(cycleWidth - pulseWidth);
 }
 //*/
-void loop() {
-  //plot channels of PPM
-  /*
-    // Print latest valid values from all channels
-    for (byte channel = 1; channel <= channelAmount; ++channel) {
-        //unsigned value = ppm.latestValidChannelValue(channel, 0);]
-        unsigned value = ppm.rawChannelValue(channel);
-        Serial.print(value);
-    if(channel < channelAmount) Serial.print('\t');
-    }
-    Serial.println();
-  delay(20);
-  */
 
-unsigned ch3= ppm.rawChannelValue(3); 
-unsigned ch7= ppm.rawChannelValue(7);
-int Step_Hz = map(ch3,1000,2000,25,5000);
-int dir = 0;
+void ROTATE() {
+  unsigned ch3 = ppm.rawChannelValue(3); //Left Throttle joystick
+  unsigned ch5 = ppm.rawChannelValue(5); //right 3 pole switch
 
-if (ch7 > 1500){
-  dir = 1;
+  int Step_Hz = map(ch3, 1000, 2000, 25, 5000);
+  int dir;
+  if (ch5 > 1750) { //if 3 pole switch is down
+    dir = 1;
+    PULSE(Step_Hz, dir);
+
+  } else if (ch5 < 1250) { //if 3 pole switch is up
+    dir = 0;
+    PULSE(Step_Hz, dir);
+
+  } else { ////if 3 pole switch is in the middle
+
+  }
 }
-
-PULSE(Step_Hz,dir);
-
-
-/*
-int Hz_Geared=100;
-int Hz_Motor = Hz_Geared*15.3;
-float microbetweenSteps=(1.0/Hz_Motor)*1000000;
-    
-    //digitalWrite(DIR,);
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(pulseWidth);
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(microbetweenSteps);
-    */
+void loop() {
+  unsigned ch7 = ppm.rawChannelValue(7); //Left 2 pole switch
+  while (ch7 < 1250) { // if the 2 pole switch is down, rotate constantly
+    ROTATE();
+  }
+  if (ch7 > 1750) {
+    float i = 0;
+    unsigned ch4;
+    float targetPos;
+    float e;
+    while (ch7 > 1750) { // if the 2 pole switch is up, go to position
+      ch4 = ppm.rawChannelValue(4); //Left yaw joystick
+      targetPos = map(ch4, 1000, 2000, -100, 100);
+      e = i - targetPos;
+      if (e > 0) {
+        PULSE(1000, 1);
+        i += 1;
+      }
+      if (e < 0) {
+        PULSE(1000, 0);
+        i -= 1;
+      }
+    }
+  }
+  if ( ch7 > 1250 && ch7 < 1750) {
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+  }
 }
