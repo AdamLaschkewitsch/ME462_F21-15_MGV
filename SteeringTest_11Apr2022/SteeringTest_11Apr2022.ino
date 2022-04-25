45fce1c5d5498daa7f860dc6e65509317143e734
AdamLaschkewitsch <98042645+AdamLaschkewitsch@users.noreply.github.com>
2022-04-25 15:21:46 -0500

    Modified to Use Potentiometer Instead of Radio
    Radio Reciever died, so needed to use a potentiometer to control the motor for testing and video.

--- SteeringTest_11Apr2022/SteeringTest_11Apr2022.ino
+++ SteeringTest_11Apr2022/SteeringTest_11Apr2022.ino
@@ -33,7 +33,13 @@ float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
 }
 
 //---------------------------
+unsigned CH(int channel) {
+ //unsigned value =  ppm.rawChannelValue(channel);
+ unsigned value = map(analogRead(A0),0,255,1000,2000);
+ return value;
+}
 
+//---------------------------
 void PULSE(float RPM, int dir) {
 
   float stepFreq = RPM / 60 * stepsPerRev * gearRatio;
@@ -50,8 +56,8 @@ void PULSE(float RPM, int dir) {
 //---------------------------
 
 int ROTATE(int currentPos) {
-  unsigned ch3 = ppm.rawChannelValue(3); //Left Throttle joystick
-  unsigned ch5 = ppm.rawChannelValue(5); //right 3 pole switch
+  unsigned ch3 = CH(3); //Left Throttle joystick
+  unsigned ch5 = CH(5); //right 3 pole switch
 
   float RPM = mapfloat(ch3, 1000, 2000, 0, maxGearboxRPM); // min and max gearbox rpm
   if (RPM > 0) {
@@ -81,7 +87,7 @@ int posCONTROL(int Position) {
   float targetPos;
   float e;
   int i;
-  ch4 = ppm.rawChannelValue(4); //Left yaw joystick
+  ch4 = CH(4); //Left yaw joystick
   targetPos = map(ch4, 1000, 2000, maxCW, maxCCW);
   e = targetPos - currentPos;
   //Serial.println(e);
@@ -115,28 +121,30 @@ void setup() {
   currentPos=0;
 }
 void loop() {
-  ch7 = ppm.rawChannelValue(7); //Left 2 pole switch
-  //Serial.println(ch7);
+/*
+  ch7 = CH(7); //Left 2 pole switch
+ 
   //  currentPos = currentPos % (gearRatio * stepsPerRotation * rangeOfRotation / 2);
   while (ch7 < 1250) { // if the 2 pole switch is down, rotate constantly
-    ch7 = ppm.rawChannelValue(7); //update ch7
+    ch7 = CH(7); //update ch7
     currentPos = ROTATE(currentPos);
     //Serial.println(i);
 
   }
 
   while (ch7 > 1750) { // if the 2 pole switch is up, control position
-    ch7 = ppm.rawChannelValue(7); //update ch7
+    ch7 = CH(7); //update ch7
+    //*/
     currentPos = posCONTROL(currentPos);
     // Serial.println(i);
 
-  }
-
+  //}
+/*
   if ( ch7 > 1250 && ch7 < 1750) { // if invalid signal is detected, only flash led
     digitalWrite(13, HIGH);
     delay(200);
     digitalWrite(13, LOW);
     delay(200);
     // Serial.println(i);
-  }
+  }//*/
 }
